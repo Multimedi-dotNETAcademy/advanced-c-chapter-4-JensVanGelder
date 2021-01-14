c64e94dbb0f3c2e539b918f8e837b68132a5b8be
JensVanGelder <jens.v.gelder@gmail.com>
2021-01-14 16:45:57 +0100

    Finished Prijzen Foreach

--- PrijzenMetForeach/Form1.Designer.cs
+++ PrijzenMetForeach/Form1.Designer.cs
@@ -29,13 +29,110 @@ namespace PrijzenMetForeach
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.lblHigher = new System.Windows.Forms.Label();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(45, 70);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(169, 68);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Enabled = false;
+            this.btnCalc.Location = new System.Drawing.Point(45, 129);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(127, 23);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "Show higher than €5";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Enabled = false;
+            this.btnAverage.Location = new System.Drawing.Point(45, 169);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(127, 23);
+            this.btnAverage.TabIndex = 3;
+            this.btnAverage.Text = "Show average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(200, 174);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(53, 13);
+            this.lblAverage.TabIndex = 4;
+            this.lblAverage.Text = "Average: ";
+            // 
+            // lblHigher
+            // 
+            this.lblHigher.AutoSize = true;
+            this.lblHigher.Location = new System.Drawing.Point(200, 134);
+            this.lblHigher.Name = "lblHigher";
+            this.lblHigher.Size = new System.Drawing.Size(44, 13);
+            this.lblHigher.TabIndex = 5;
+            this.lblHigher.Text = "Higher: ";
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(42, 45);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(44, 13);
+            this.lblInput.TabIndex = 6;
+            this.lblInput.Text = "Prijzen; ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(783, 250);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.lblHigher);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Prijzen met Foreach (Enter 20 Numbers)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label lblHigher;
+        private System.Windows.Forms.Label lblInput;
     }
 }
 
--- PrijzenMetForeach/Form1.cs
+++ PrijzenMetForeach/Form1.cs
@@ -12,9 +12,51 @@ namespace PrijzenMetForeach
 {
     public partial class Form1 : Form
     {
+        double[] prices = new double[20];
+        List<double> input = new List<double>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lblInput.Text += $"{txtInput.Text}, ";
+            input.Add(Convert.ToDouble(txtInput.Text));
+            if (input.Count == 20)
+            {
+                btnAdd.Enabled = false;
+                prices = input.ToArray();
+                btnAverage.Enabled = true;
+                btnCalc.Enabled = true;
+            }
+        }
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+            double average = 0.0;
+            foreach (double price in prices)
+            {
+                average += price;
+            }
+            average = average / 20;
+            lblAverage.Text += $"{average}";
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            foreach (double price in prices)
+            {
+                if (price > 5.0)
+                {
+                    lblHigher.Text += $"{price}, ";
+                }
+            }
         }
     }
 }
